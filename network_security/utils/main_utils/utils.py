e29ed0c8d34e58be1871f7a559def54115010379
Karan Prajapat <karan.prajapat.ug22@nsut.ac.in>
2025-06-20 02:52:41 +0100

    Model Trainer part completed

--- network_security/utils/main_utils/utils.py
+++ network_security/utils/main_utils/utils.py
@@ -1,6 +1,3 @@
-import os
-
-# import dill
 import pickle
 import sys
 from pathlib import Path
@@ -31,16 +28,16 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
         raise NetworkSecurityException(e, sys)
 
 
-def save_numpy_array_data(file_path: str, array: np.array):
+def save_numpy_array_data(file_path: str, array: np.array) -> None:
     """
     Save numpy array data to file
     file_path: str location of file to save
-    array: np.array data to save
+    array: np.array data to save.
     """
     try:
-        dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)
-        with open(file_path, "wb") as file_obj:
+        dir_path = Path(file_path).parent
+        dir_path.mkdir(parents=True, exist_ok=True)
+        with Path(file_path).open("wb") as file_obj:
             np.save(file_obj, array)
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
@@ -49,9 +46,34 @@ def save_numpy_array_data(file_path: str, array: np.array):
 def save_object(file_path: str, obj: object) -> None:
     try:
         logging.info("Entered the save_object method of MainUtils class")
-        os.makedirs(os.path.dirname(file_path), exist_ok=True)
-        with open(file_path, "wb") as file_obj:
+        Path(file_path).parent.mkdir(parents=True, exist_ok=True)
+        with Path(file_path).open("wb") as file_obj:
             pickle.dump(obj, file_obj)
         logging.info("Exited the save_object method of MainUtils class")
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
+
+
+def load_object(file_path: str) -> object:
+    try:
+        if not Path(file_path).exists():
+            raise Exception(f"The file: {file_path} is not exists")
+        with Path(file_path).open("rb") as file_obj:
+            print(file_obj)
+            return pickle.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
+
+def load_numpy_array_data(file_path: str) -> np.array:
+    """
+    Load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded.
+    """
+    try:
+        with Path(file_path).open("rb") as file_obj:
+            return np.load(file_obj)
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
