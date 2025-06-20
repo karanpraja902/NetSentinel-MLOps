ff347ffe7f330587978d62384d7c369cd068fb1c
Karan Prajapat <karan.prajapat.ug22@nsut.ac.in>
2025-06-20 03:14:25 +0100

    MLflow implementation

--- network_security/components/model_trainer.py
+++ network_security/components/model_trainer.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
 
+import mlflow
 from sklearn.ensemble import (
     AdaBoostClassifier,
     GradientBoostingClassifier,
@@ -40,6 +41,17 @@ class ModelTrainer:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
+    def track_mlflow(self, best_model, classificationmetric):
+        with mlflow.start_run():
+            f1_score = classificationmetric.f1_score
+            precision_score = classificationmetric.precision_score
+            recall_score = classificationmetric.recall_score
+
+            mlflow.log_metric("f1_score", f1_score)
+            mlflow.log_metric("precision", precision_score)
+            mlflow.log_metric("recall_score", recall_score)
+            mlflow.sklearn.log_model(best_model, "model")
+
     def train_model(
         self,
         X_train: object,
@@ -57,20 +69,20 @@ class ModelTrainer:
         params = {
             "Decision Tree": {
                 "criterion": ["gini", "entropy", "log_loss"],
-                "splitter": ["best", "random"],
-                "max_features": ["sqrt", "log2"],
+                # "splitter": ["best", "random"],
+                # "max_features": ["sqrt", "log2"],
             },
             "Random Forest": {
-                "criterion": ["gini", "entropy", "log_loss"],
-                "max_features": ["sqrt", "log2", None],
+                # "criterion": ["gini", "entropy", "log_loss"],
+                # "max_features": ["sqrt", "log2", None],
                 "n_estimators": [8, 16, 32, 128, 256],
             },
             "Gradient Boosting": {
-                "loss": ["log_loss", "exponential"],
+                # "loss": ["log_loss", "exponential"],
                 "learning_rate": [0.1, 0.01, 0.05, 0.001],
                 "subsample": [0.6, 0.7, 0.75, 0.85, 0.9],
-                "criterion": ["squared_error", "friedman_mse"],
-                "max_features": ["auto", "sqrt", "log2"],
+                # "criterion": ["squared_error", "friedman_mse"],
+                # "max_features": ["auto", "sqrt", "log2"],
                 "n_estimators": [8, 16, 32, 64, 128, 256],
             },
             "Logistic Regression": {},
@@ -102,12 +114,16 @@ class ModelTrainer:
             y_true=y_train,
             y_pred=y_train_pred,
         )
+        ## Track the training experiements with mlflow
+        self.track_mlflow(best_model, classification_train_metric)
 
         y_test_pred = best_model.predict(X_test)
         classification_test_metric = get_classification_score(
             y_true=y_test,
             y_pred=y_test_pred,
         )
+
+        self.track_mlflow(best_model, classification_test_metric)
 
         preprocessor = load_object(
             file_path=self.data_transformation_artifact.transformed_object_file_path,
